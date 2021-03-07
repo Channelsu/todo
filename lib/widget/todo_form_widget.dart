e2ba8b3c79c491f2142f93c29b18c77039cf672d
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-07 22:24:35 +0900

    保存ボタン作成

--- lib/widget/todo_form_widget.dart
+++ lib/widget/todo_form_widget.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 
 class TodoFormWidget extends StatelessWidget {
+  final String title;
+  final String description;
+  final ValueChanged<String> onChangedTitle;
+  final ValueChanged<String> onChangedDescription;
+  final VoidCallback onSavedTodo;
+
+  // コンストラクタ
+  const TodoFormWidget({
+    Key key,
+    this.title = '',
+    this.description = '',
+    @required this.onChangedTitle,
+    @required this.onChangedDescription,
+    @required this.onSavedTodo,
+  });
+
+
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      
-    );
-  }
+  Widget build(BuildContext context) => SingleChildScrollView(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        buildButton(),
+      ],
+    ),
+  );
+
+  Widget buildButton() => SizedBox(
+    width: double.infinity,
+    child: RaisedButton(
+      color: Colors.black,
+      textColor: Colors.white,
+      onPressed: onSavedTodo,
+      child: Text('追加'),
+    ),
+  );
+
 }
