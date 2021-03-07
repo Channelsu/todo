bb21236a10bee7558dd6c4e890a16266bca28111
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-07 19:56:15 +0900

    ボトムナビゲーションバー作成

---
+++ lib/widget/add_todo_dialog_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddTodoDialogWidget extends StatefulWidget {
+  @override
+  _AddTodoDialogWidgetState createState() => _AddTodoDialogWidgetState();
+}
+
+class _AddTodoDialogWidgetState extends State<AddTodoDialogWidget> {
+  final _formKey = GlobalKey<FormState>();
+  String title = '';
+  String description = '';
+
+  @override
+  Widget build(BuildContext context) => AlertDialog(
+
+  );
+}
