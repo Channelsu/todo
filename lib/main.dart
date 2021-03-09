180e4bfba6d4de0c7ea0a5d2c1f92e131b9829ba
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-09 22:25:55 +0900

    ChangeNotifierProviderでMaterialAppを覆う処理記述

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:todo/page/home_page.dart';
+import 'package:todo/provider/todo.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,13 +9,16 @@ class MyApp extends StatelessWidget {
   static final String title = 'Todoアプリ';
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: title,
-    theme: ThemeData(
-      primarySwatch: Colors.teal,
-      scaffoldBackgroundColor: Color(0xFF6f5ee),
+  Widget build(BuildContext context) => ChangeNotifierProvider(
+    create: (context) => TodosProvider(),
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: title,
+      theme: ThemeData(
+        primarySwatch: Colors.teal,
+        scaffoldBackgroundColor: Color(0xFFf6f5ee),
+      ),
+      home: HomePage(),
     ),
-    home: HomePage(),
   );
 }
