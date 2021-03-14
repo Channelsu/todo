e8d7ede016e3313b3981324965b9704afff7a154
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-14 22:40:23 +0900

    todo追加ボタン押下時の処理作成

--- lib/widget/add_todo_dialog_widget.dart
+++ lib/widget/add_todo_dialog_widget.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/model/todo.dart';
+import 'package:todo/provider/todos.dart';
 import 'package:todo/widget/todo_form_widget.dart';
 
 class AddTodoDialogWidget extends StatefulWidget {
@@ -13,20 +16,47 @@ class _AddTodoDialogWidgetState extends State<AddTodoDialogWidget> {
 
   @override
   Widget build(BuildContext context) => AlertDialog(
-    content: Column(
-      // Columnはデフォルトで画面の最大の高さまで広がってしまう
-      // 軸方向のサイズを最小に指定すればぴったりサイズになる
-      mainAxisSize: MainAxisSize.min,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('todoの追加',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
-        SizedBox(height: 12,),
-        TodoFormWidget(
-          onChangedTitle: (title) => setState(() => this.title = title),
-          onChangedDescription: (description) => setState(() => this.description = description),
-          onSavedTodo: () {},
-        ),
-      ],
+    content: Form(
+      key: _formKey,
+      child: Column(
+        // Columnはデフォルトで画面の最大の高さまで広がってしまう
+        // 軸方向のサイズを最小に指定すればぴったりサイズになる
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('todoの追加',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),),
+          SizedBox(height: 12,),
+          TodoFormWidget(
+            onChangedTitle: (title) => setState(() => this.title = title),
+            onChangedDescription: (description) => setState(() => this.description = description),
+            onSavedTodo: addTodo,
+          ),
+        ],
+      ),
     ),
   );
+
+  // 追加ボタン押下時の処理
+  void addTodo() {
+    final isValid = _formKey.currentState.validate();
+
+    if(!isValid) {
+      return;
+    } else {
+      // Todoインスタンス作成
+      final todo = Todo(
+        id: DateTime.now().toString(),
+        title: title,
+        description: description,
+        createdTime: DateTime.now(),
+      );
+
+      // providerのaddTodoメソッド呼び出し
+      final provider = Provider.of<TodosProvider>(context, listen: false);
+      provider.addTodo(todo);
+
+      // 戻る画面遷移(ダイアログを閉じる)
+      Navigator.of(context).pop();
+    }
+  }
 }
