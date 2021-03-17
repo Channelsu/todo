3ccfdc7198c52ee8597b60208ba9e95bcbf56458
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-17 23:56:33 +0900

    todo完了ページの土台作成

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo/main.dart';
 import 'package:todo/widget/add_todo_dialog_widget.dart';
+import 'package:todo/widget/completed_list_widget.dart';
 import 'package:todo/widget/todo_list_widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,7 +16,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     final tabs = [
       TodoListWidget(),
-      Container(),
+      CompletedListWidget(),
     ];
 
     return Scaffold(
---
+++ lib/widget/completed_list_widget.dart
@@ -0,0 +1,11 @@
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo/provider/todos.dart';
+
+class CompletedListWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+  }
+}
