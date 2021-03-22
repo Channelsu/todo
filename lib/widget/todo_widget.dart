2209c9b91f5ccdf5d89de750699f30b70522c793
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-03-22 22:14:29 +0900

    todoをタップすると編集ページに遷移するよう実装

--- lib/widget/todo_widget.dart
+++ lib/widget/todo_widget.dart
@@ -42,46 +42,49 @@ class TodoWidget extends StatelessWidget {
     ),
   );
 
-  Widget buildTodo(BuildContext context) => Container(
-    color: Colors.white,
-    padding: EdgeInsets.all(20),
-    child: Row(
-      children: [
-        Checkbox(
-          activeColor: Theme.of(context).primaryColor,
-          checkColor: Colors.white,
-          value: todo.isDone,
-          onChanged: (_) {
-            final provider = Provider.of<TodosProvider>(context, listen: false);
-            final isDone = provider.toggleTodoStatus(todo);
-            Utils.showSnackBar(context, isDone ? '完了しました' : 'Todoリストに移動しました');
-          },
-        ),
-        SizedBox(height: 20,),
-        Expanded(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                todo.title,
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 22,
-                ),
-              ),
-              if(todo.description.isNotEmpty)
-                Container(
-                  margin: EdgeInsets.only(top: 4),
-                  child: Text(
-                    todo.description,
-                    style: TextStyle(fontSize: 20, height: 1.5),
-                  ),
-                )
-            ],
+  Widget buildTodo(BuildContext context) => GestureDetector(
+    onTap: () => editTodo(context, todo),
+    child: Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(20),
+      child: Row(
+        children: [
+          Checkbox(
+            activeColor: Theme.of(context).primaryColor,
+            checkColor: Colors.white,
+            value: todo.isDone,
+            onChanged: (_) {
+              final provider = Provider.of<TodosProvider>(context, listen: false);
+              final isDone = provider.toggleTodoStatus(todo);
+              Utils.showSnackBar(context, isDone ? '完了しました' : 'Todoリストに移動しました');
+            },
           ),
-        ),
-      ],
+          SizedBox(height: 20,),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  todo.title,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 22,
+                  ),
+                ),
+                if(todo.description.isNotEmpty)
+                  Container(
+                    margin: EdgeInsets.only(top: 4),
+                    child: Text(
+                      todo.description,
+                      style: TextStyle(fontSize: 20, height: 1.5),
+                    ),
+                  )
+              ],
+            ),
+          ),
+        ],
+      ),
     ),
   );
 
