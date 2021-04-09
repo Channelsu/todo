e5dc5a5edf950f02d64bc8d44bbccbda03afbfe4
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-04-10 00:04:11 +0900

    _todosの中を空に

--- lib/provider/todos.dart
+++ lib/provider/todos.dart
@@ -4,20 +4,11 @@ import 'package:todo/model/todo.dart';
 
 class TodosProvider extends ChangeNotifier {
   List<Todo> _todos = [
-    Todo(
-      title: '買い物',
-      description: '買い物詳細',
-      createdTime: DateTime.now(),
-    ),
-    Todo(
-      title: 'スケジュール',
-      description: 'スケジュール詳細',
-      createdTime: DateTime.now(),
-    ),
-    Todo(
-      title: '勉強する',
-      createdTime: DateTime.now(),
-    ),
+    // Todo(
+    //   title: '買い物',
+    //   description: '買い物詳細',
+    //   createdTime: DateTime.now(),
+    // ),
   ];
 
   List<Todo> get todos => _todos.where((todo) => todo.isDone == false).toList();
