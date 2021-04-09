bdb20b84f37cb60d8a632f8de20eac817573ea94
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-04-09 22:44:11 +0900

    json形式からtodoオブジェクトに変換する関数作成

--- lib/model/todo.dart
+++ lib/model/todo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:todo/utils.dart';
 
 class TodoField {
   static const createdTime = 'createdTime';
@@ -19,4 +20,22 @@ class Todo {
     this.isDone = false,
     @required this.createdTime,
   });
+
+  //
+  static Todo fromJson(Map<String, dynamic> json) => Todo(
+    id: json['id'],
+    title: json['title'],
+    description: json['description'],
+    isDone: json['isDone'],
+    createdTime: Utils.toDateTime(json['createdTime']),
+  );
+
+  // json形式へと変換する関数
+  Map<String, dynamic> toJson() => {
+    'id': id,
+    'title': title,
+    'description': description,
+    'isDone': isDone,
+    'createdTime': Utils.fromDateTimeToJson(createdTime),
+  };
 }
