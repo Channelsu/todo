56733bddab81fecdec2b780570191e007ba9299c
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-04-09 22:11:38 +0900

    日時へと変換する関数作成

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -6,6 +7,12 @@ class Utils {
     Scaffold.of(context)
       ..removeCurrentSnackBar()
       ..showSnackBar(SnackBar(content: Text(text),));
+
+  // 日時へと変換する関数
+  static DateTime toDateTime(Timestamp value) {
+    if (value == null) return null;
+    return value.toDate();
+  }
 
   // 日時をjsonに変換する関数
   static dynamic fromDateTimeToJson(DateTime date) {
